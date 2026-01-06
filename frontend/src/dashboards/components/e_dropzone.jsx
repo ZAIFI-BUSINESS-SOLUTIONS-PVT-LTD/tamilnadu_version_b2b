6ca5b27f0f88e4c52d715a275a5f2e6b0fde49e7
BharathShanmukham <bharath.nimme@gmail.com>
2026-01-06 07:04:50 +0530

    Sync frontend from dev (mirror including deletions)

--- frontend/src/dashboards/components/e_dropzone.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import DropZone from './ui/dropzone.jsx';
-
-// Wrapper for backward compatibility if needed
-const EducatorDropZone = (props) => <DropZone {...props} />;
-
-export default React.memo(EducatorDropZone);
